a81c2fd09e7b88be3298057cf3ac85f156aec310
vchirakarn saetang <ozone.ap23@gmail.com>
2021-12-21 22:37:22 +0700

    no bugs now

--- main.ts
+++ main.ts
@@ -1,16 +1,16 @@
 function button () {
     buttonVal = pins.analogReadPin(AnalogPin.P2)
-    if (buttonVal < 256) {
+    if (buttonVal < 256 && buttonVal >= 1) {
         item = 1
-    } else if (buttonVal < 597) {
+    } else if (buttonVal < 597 && buttonVal >= 256) {
         item = 2
-    } else if (buttonVal < 725) {
+    } else if (buttonVal < 725 && buttonVal >= 597) {
         item = 3
-    } else if (buttonVal < 793) {
+    } else if (buttonVal < 793 && buttonVal >= 725) {
         item = 4
-    } else if (buttonVal < 836) {
+    } else if (buttonVal < 836 && buttonVal >= 793) {
         item = 5
-    } else if (buttonVal < 938) {
+    } else if (buttonVal < 938 && buttonVal >= 836) {
         item = 6
     } else {
         item = 0
@@ -48,7 +48,7 @@ basic.forever(function () {
     } else if (pins.analogReadPin(AnalogPin.P0) <= 199 && pins.analogReadPin(AnalogPin.P0) >= 100) {
         radio.sendNumber(9)
         basic.showNumber(-75)
-    } else if (pins.analogReadPin(AnalogPin.P0) <= 99 && pins.analogReadPin(AnalogPin.P0) >= 0) {
+    } else if (pins.analogReadPin(AnalogPin.P0) <= 99 && pins.analogReadPin(AnalogPin.P0) >= 1) {
         radio.sendNumber(10)
         basic.showNumber(-100)
     } else if (pins.analogReadPin(AnalogPin.P0) >= 601 && pins.analogReadPin(AnalogPin.P0) <= 700) {
@@ -93,7 +93,7 @@ basic.forever(function () {
             . # . . #
             # # . # .
             `)
-    } else if (pins.analogReadPin(AnalogPin.P1) <= 99 && pins.analogReadPin(AnalogPin.P1) >= 0) {
+    } else if (pins.analogReadPin(AnalogPin.P1) <= 99 && pins.analogReadPin(AnalogPin.P1) >= 1) {
         radio.sendNumber(18)
         basic.showNumber(100)
         basic.showLeds(`
